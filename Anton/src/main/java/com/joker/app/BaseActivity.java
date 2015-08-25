bd5def53d994d91271bbe277a2088c2a4514be36
deng wei <1170859911@qq.com>
2015-08-26 00:05:50 +0800

    test2

--- Anton/src/main/java/com/joker/app/BaseActivity.java
+++ Anton/src/main/java/com/joker/app/BaseActivity.java
@@ -10,8 +10,6 @@ public class BaseActivity extends AppCompatActivity {
 
   @Override public void setContentView(int layoutResID) {
     super.setContentView(layoutResID);
-
-    /**/
     ButterKnife.bind(BaseActivity.this);
   }
 
